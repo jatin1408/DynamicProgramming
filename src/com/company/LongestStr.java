e3df4eef5c1ba6097fd9cac08a888187da77301f
Jatin Thakwani <47001242+jatin1408@users.noreply.github.com>
2020-08-13 21:44:21 +0530

    "Added Longest Common Substring"

---
+++ src/com/company/LongestStr.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class LongestStr {
+    public static int longest(String a,String b){
+        int m=a.length();
+        int n=b.length();
+
+        int[][] t=new int[m+1][n+1];
+        int result=0;
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if (i == 0 || j == 0)
+                    t[i][j] = 0;
+                if(a.charAt(i-1)==b.charAt(j-1)){
+                    t[i][j]=1+t[i-1][j-1];
+                    result = Integer.max(result, t[i][j]);
+                }
+                else {
+                    t[i][j]=0;
+                }
+            }
+        }
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                System.out.print(t[i][j]+" ");
+            }
+            System.out.println();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String a="abcde";
+        String b="abfce";
+        System.out.println(longest(a,b));
+    }
+}
