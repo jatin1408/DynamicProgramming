549937f954d3c3c4293f1f8a7e746e9bb8e70d12
Jatin Thakwani <47001242+jatin1408@users.noreply.github.com>
2020-08-15 20:54:22 +0530

    Added Word Break

---
+++ src/com/company/WordBreak.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> hashset=new HashSet<>();
+        for(String obj:wordDict){
+            hashset.add(obj);
+        }
+        HashMap<String,Boolean> hashmap=new HashMap<>();
+        return findWord(s,hashset,hashmap);
+    }
+    public boolean findWord(String s, HashSet<String> words, HashMap<String,Boolean> hashMap){
+        if(s.length()==0){
+            return true;
+        }
+        if(hashMap.containsKey(s)){
+            return hashMap.get(s);
+        }
+        for(int i=1;i<=s.length();i++){
+            if(words.contains(s.substring(0,i)) && findWord(s.substring(i,s.length()),words,hashMap)){
+                hashMap.put(s.substring(i,s.length()),true);
+                return true;
+            }
+            hashMap.put(s,false);
+        }
+        return false;
+    }
+}
